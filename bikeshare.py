545209ddee987d14eb41730021840efb960d9cf8
Fady <ffadyffayez@gmail.com>
2021-04-17 21:26:30 +0200

    Load_data() function Docstring modification

--- bikeshare.py
+++ bikeshare.py
@@ -56,7 +56,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city, month and day (if specified)
 
     Args:
         (str) city - name of the city to analyze
